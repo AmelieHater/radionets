e273c226bd57ec68fbd517b6c6b28276d9e09a89
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-07-09 15:33:58 +0200

    Add radionets custom PuOr colormap

--- src/radionets/__init__.py
+++ src/radionets/__init__.py
@@ -4,6 +4,13 @@ raionets - Deep Learning-based imaging in radio interferometry
 Licensed under a MIT style license - see LICENSE
 """
 
+from matplotlib import colormaps
+
+from radionets.plotting._puor import PuOr, PuOr_r
+
 from .version import __version__
 
 __all__ = ["__version__"]
+
+colormaps.register(cmap=PuOr)
+colormaps.register(cmap=PuOr_r)
