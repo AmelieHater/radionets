3f68c34f7f55633d6f538cc9b9671f67e0e87210
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-11-27 17:05:35 +0100

    Add logging handler main class

--- src/radionets/core/logging.py
+++ src/radionets/core/logging.py
@@ -1,9 +1,67 @@
 import logging
 
+from lightning.pytorch.loggers import CSVLogger
+from pydantic import BaseModel
 from rich.logging import RichHandler
 
+logging.basicConfig(
+    level="INFO",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
 
-def setup_logger(namespace="rich", level="INFO", **kwargs):
+
+class Loggers:
+    @classmethod
+    def get_loggers(cls, train_config: BaseModel) -> list:
+        default_logger = CSVLogger(
+            save_dir=train_config.paths.model_path,
+            **train_config.logging.default_logger.model_dump(),
+        )
+        default_logger._name = train_config.logging.project_name
+
+        loggers = [default_logger]
+
+        if train_config.logging.comet_ml:
+            try:
+                from lightning.pytorch.loggers import CometLogger
+            except ImportError as e:
+                raise ModuleNotFoundError(
+                    "'comet_ml' was set to 'true' in your training config but "
+                    "radionets could not import 'CometLogger'. This usually "
+                    "indicates that 'comet_ml' is missing from your environment. "
+                    "You can install it using 'uv pip install comet_ml'."
+                ) from e
+
+            comet_logger = CometLogger(
+                project=train_config.logging.project_name,
+                **train_config.logging.comet_ml.model_dump(),
+            )
+            loggers.append(comet_logger)
+
+        if train_config.logging.mlflow:
+            try:
+                from lightning.pytorch.loggers import MLflowLogger
+            except ImportError as e:
+                raise ModuleNotFoundError(
+                    "'mlflow' was set to 'true' in your training config but "
+                    "radionets could not import 'MLflowLogger'. This usually "
+                    "indicates that 'mlflow' is missing from your environment. "
+                    "You can install it using 'uv pip install mlflow'."
+                ) from e
+
+            mlflow_logger = MLflowLogger(
+                experiment_name=train_config.logging.project_name,
+                save_dir=train_config.paths.model_path,
+                **train_config.logging.mlflow.model_dump(),
+            )
+            loggers.append(mlflow_logger)
+
+        return loggers
+
+
+def _setup_logger(namespace="rich", level="INFO", **kwargs):
     """Basic logging setup. Uses :class:`~rich.logging.RichHandler`
     for formatting and highlighting of the log.
 
