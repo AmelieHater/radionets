706f61250a08ea184dd4ab3cd8d75fa610ba13c6
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-12-17 11:29:29 +0100

    Replace carbontracker contextmanager function with class

--- src/radionets/utils/carbon_tracking.py
+++ src/radionets/utils/carbon_tracking.py
@@ -1,4 +1,4 @@
-from contextlib import contextmanager
+from typing import Self
 
 try:
     from codecarbon import OfflineEmissionsTracker
@@ -8,18 +8,35 @@ except ImportError:
     _CODECARBON_AVAILABLE = False
 
 
-__all__ = ["carbontracker"]
+__all__ = ["CarbonTracker"]
 
 
-@contextmanager
-def carbontracker(train_config):
-    if _CODECARBON_AVAILABLE and train_config.logging.codecarbon:
-        tracker = OfflineEmissionsTracker(
-            **train_config.logging.codecarbon.model_dump()
-        )
-        try:
-            yield tracker.start()
-        finally:
-            tracker.stop()
-    else:
-        yield None
+class DummyTracker:
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
+class CarbonTracker:
+    def __init__(self, train_config, stop_inside_scope=True, *args, **kwargs):
+        self.train_config = train_config
+        self.use = _CODECARBON_AVAILABLE and train_config.logging.codecarbon
+        self.stop = stop_inside_scope
+
+    def __enter__(self) -> Self:
+        if self.use:
+            self.tracker = OfflineEmissionsTracker(
+                **self.train_config.logging.codecarbon.model_dump()
+            )
+            self.tracker.start()
+        else:
+            self.tracker = DummyTracker()
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        if self.stop:
+            self.tracker.stop()
+        return None
